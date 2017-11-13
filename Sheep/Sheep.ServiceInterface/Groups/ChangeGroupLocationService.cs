a15524367489be22643d3be8361e76cf7c426ed0
洪立宇 <hongliyu2002@hotmail.com>
2017-11-13 23:02:08 +0800

    新增群组修改地址

---
+++ Sheep/Sheep.ServiceInterface/Groups/ChangeGroupLocationService.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ServiceStack;
+using ServiceStack.Configuration;
+using ServiceStack.FluentValidation;
+using ServiceStack.Logging;
+using ServiceStack.Validation;
+using Sheep.Model.Corp;
+using Sheep.Model.Corp.Entities;
+using Sheep.ServiceInterface.Properties;
+using Sheep.ServiceModel.Groups;
+
+namespace Sheep.ServiceInterface.Groups
+{
+    /// <summary>
+    ///     更改所在地服务接口。
+    /// </summary>
+    public class ChangeGroupLocationService : Service
+    {
+        #region 静态变量
+
+        /// <summary>
+        ///     相关的日志记录器。
+        /// </summary>
+        protected static readonly ILog Log = LogManager.GetLogger(typeof(ChangeGroupLocationService));
+
+        #endregion
+
+        #region 属性 
+
+        /// <summary>
+        ///     获取及设置相关的应用程序设置器。
+        /// </summary>
+        public IAppSettings AppSettings { get; set; }
+
+        /// <summary>
+        ///     获取及设置更改所在地的校验器。
+        /// </summary>
+        public IValidator<GroupChangeLocation> GroupChangeLocationValidator { get; set; }
+
+        /// <summary>
+        ///     获取及设置群组的存储库。
+        /// </summary>
+        public IGroupRepository GroupRepo { get; set; }
+
+        #endregion
+
+        #region 更改所在地
+
+        /// <summary>
+        ///     更改所在地。
+        /// </summary>
+        public async Task<object> Put(GroupChangeLocation request)
+        {
+            if (!IsAuthenticated)
+            {
+                throw HttpError.Unauthorized(Resources.LoginRequired);
+            }
+            if (HostContext.GlobalRequestFilters == null || !HostContext.GlobalRequestFilters.Contains(ValidationFilters.RequestFilter))
+            {
+                GroupChangeLocationValidator.ValidateAndThrow(request, ApplyTo.Put);
+            }
+            var existingGroup = await GroupRepo.GetGroupAsync(request.GroupId);
+            if (existingGroup == null)
+            {
+                throw HttpError.NotFound(string.Format(Resources.GroupNotFound, request.GroupId));
+            }
+            var currentUserId = GetSession().UserAuthId.ToInt(0);
+            if (currentUserId != existingGroup.OwnerId)
+            {
+                throw HttpError.Unauthorized(Resources.GroupOwnerRequired);
+            }
+            var newGroup = new Group();
+            newGroup.PopulateWith(existingGroup);
+            newGroup.Meta = existingGroup.Meta == null ? new Dictionary<string, string>() : new Dictionary<string, string>(existingGroup.Meta);
+            newGroup.Country = request.Country;
+            newGroup.State = request.State;
+            newGroup.City = request.City;
+            var group = await GroupRepo.UpdateGroupAsync(existingGroup, newGroup);
+            Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("date:res:/groups/{0}", group.Id)).ToArray());
+            Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("res:/groups/{0}", group.Id)).ToArray());
+            Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("date:res:/groups/basic/{0}", group.Id)).ToArray());
+            Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("res:/groups/basic/{0}", group.Id)).ToArray());
+            if (!group.RefId.IsNullOrEmpty())
+            {
+                Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("date:res:/groups/show/{0}", group.RefId)).ToArray());
+                Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("res:/groups/show/{0}", group.RefId)).ToArray());
+                Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("date:res:/groups/basic/show/{0}", group.RefId)).ToArray());
+                Request.RemoveFromCache(Cache, Cache.GetKeysStartingWith(string.Format("res:/groups/basic/show/{0}", group.RefId)).ToArray());
+            }
+            return new GroupChangeLocationResponse();
+        }
+
+        #endregion
+    }
+}
--- Sheep/Sheep.ServiceModel/Groups/GroupChangeLocation.cs
+++ Sheep/Sheep.ServiceModel/Groups/GroupChangeLocation.cs
@@ -1,31 +1,37 @@
 ﻿using System.Runtime.Serialization;
 using ServiceStack;
 
-namespace Sheep.ServiceModel.Accounts
+namespace Sheep.ServiceModel.Groups
 {
     /// <summary>
     ///     更改所在地的请求。
     /// </summary>
-    [Route("/account/location", HttpMethods.Put)]
+    [Route("/groups/{GroupId}/location", HttpMethods.Put)]
     [DataContract]
-    public class AccountChangeLocation : IReturn<AccountChangeLocationResponse>
+    public class GroupChangeLocation : IReturn<GroupChangeLocationResponse>
     {
+        /// <summary>
+        ///     群组的编号。
+        /// </summary>
+        [DataMember(Order = 1, IsRequired = true)]
+        public string GroupId { get; set; }
+
         /// <summary>
         ///     更改的所在地国家/地区。
         /// </summary>
-        [DataMember(Order = 1)]
+        [DataMember(Order = 2)]
         public string Country { get; set; }
 
         /// <summary>
         ///     更改的所在地省份/直辖市/州。
         /// </summary>
-        [DataMember(Order = 2)]
+        [DataMember(Order = 3)]
         public string State { get; set; }
 
         /// <summary>
         ///     更改的所在地城市/区域。
         /// </summary>
-        [DataMember(Order = 3)]
+        [DataMember(Order = 4)]
         public string City { get; set; }
     }
 
@@ -33,7 +39,7 @@ namespace Sheep.ServiceModel.Accounts
     ///     更改所在地的响应。
     /// </summary>
     [DataContract]
-    public class AccountChangeLocationResponse : IHasResponseStatus
+    public class GroupChangeLocationResponse : IHasResponseStatus
     {
         /// <summary>
         ///     处理响应的状态。
