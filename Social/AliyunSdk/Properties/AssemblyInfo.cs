3ca0eb16394e8df26b147f9903089583fb5f2af0
洪立宇 <hongliyu2002@hotmail.com>
2018-03-09 12:18:49 +0800

    升级到.net 4.7.1，添加极光推送及阿里云两个社交库

---
+++ Social/AliyunSdk/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Aliyun Social SDK")]
+[assembly: AssemblyDescription("Aliyun social sdk client libraries.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shanghai YangQun Culture & Media Co.,Ltd.")]
+[assembly: AssemblyProduct("AliyunSdk")]
+[assembly: AssemblyCopyright("Copyright © 2017 Shanghai YangQun Culture & Media Co.,Ltd.")]
+[assembly: AssemblyTrademark("Sheep Community")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("D38C8328-9EB4-4DA7-ABE0-995202D46C37")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Social/JPushSdk/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("JPush Social SDK")]
+[assembly: AssemblyDescription("JPush social sdk client libraries.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Shanghai YangQun Culture & Media Co.,Ltd.")]
+[assembly: AssemblyProduct("JPushSdk")]
+[assembly: AssemblyCopyright("Copyright © 2017 Shanghai YangQun Culture & Media Co.,Ltd.")]
+[assembly: AssemblyTrademark("Sheep Community")]
+[assembly: AssemblyCulture("")]
+[assembly: Guid("DCA8CB1F-9AF6-4AF4-B769-5109656FCD0F")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
